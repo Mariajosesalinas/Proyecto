93474510d023dd4b50705716e04b9d84d15cedf8
Mariajosesalinas <salinascorrea86@gmail.com>
2026-08-18 11:25:01 -0400

    agregue el archivo para cargar datos

---
+++ scripts/cargar_datos.R
@@ -0,0 +1,7 @@
+install.packages("tidyverse")
+
+casen <- read.csv("~/Func Programación/Proyecto/data/Raw/casen_sucio.csv")
+
+library(readr)
+casen2 <- read_csv("data/Raw/casen_sucio.csv")
+View(casen2)
